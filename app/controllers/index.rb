790f04696db749e7482a7daeaf5e6c56d3bf77e6
dbc-apprentice <dbc@gmail.com>
2013-05-07 16:49:49 -0700

    first commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,9 +17,9 @@ get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-
+  @user = User.find_or_create_by_username(:username => @access_token.params['screen_name'], :oauth_token => @access_token.token,
+    :oauth_secret => @access_token.secret)
   # at this point in the code is where you'll need to create your user account and store the access token
-
   erb :index
   
 end
